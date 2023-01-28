e02cccfafa133acded5644d6d920c536686ad387
Arc0926 <Arcanine0926@gmail.com>
2023-01-28 15:53:26 -0500

    Added view_posts screen

--- app_files/app/src/main/java/com/SwampHacks2023/wherestenders/MainActivity.kt
+++ app_files/app/src/main/java/com/SwampHacks2023/wherestenders/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.SwampHacks2023.wherestenders
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val goto_posts = findViewById<Button>(R.id.submit_button)
+        goto_posts.setOnClickListener{
+            val intent = Intent(this, view_posts::class.java)
+            startActivity(intent)
+        }
     }
+
 }
