71100abbb8c67adc8e49df0ec9d8ef34155a4ba8
Allen Sarmiento <allenbsarmiento01@gmail.com>
2023-01-29 09:23:58 -0500

    Testing Firestore

--- app_files/app/src/main/java/com/SwampHacks2023/wherestenders/MainActivity.kt
+++ app_files/app/src/main/java/com/SwampHacks2023/wherestenders/MainActivity.kt
@@ -3,7 +3,10 @@ package com.SwampHacks2023.wherestenders
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,13 +16,19 @@ class MainActivity : AppCompatActivity() {
 
         val goto_posts = findViewById<Button>(R.id.submit_button)
         goto_posts.setOnClickListener{
-            val intent = Intent(this, PostActivity::class.java)
-            startActivity(intent)
-        }
 
-        val goto_firestore = findViewById<Button>(R.id.FirestoreBtn)
-        goto_firestore.setOnClickListener {
-            val intent = Intent(this, FirestoreTestActivity::class.java)
+            val db = Firebase.firestore
+            val data = hashMapOf("username" to "johndoe123")
+            db.collection("users")
+                .add(data)
+                .addOnSuccessListener { documentReference ->
+                    Log.d("ADD", "DocumentSnapshot written with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w("ADD", "Error adding document", e)
+                }
+
+            val intent = Intent(this, PostActivity::class.java)
             startActivity(intent)
         }
     }
